5f3eaeb4ac88038525666d731015370699353998
Joao Marcelo <joaomarcelo586@gmail.com>
2026-01-08 10:28:57 -0300

    feat: Criando classe Carro e um exemplo

---
+++ src/main/java/io/github/MarceloJoao/arquiteturaspring/montadora/HondaHRV.java
@@ -0,0 +1,14 @@
+package io.github.MarceloJoao.arquiteturaspring.montadora;
+
+import java.awt.*;
+
+public class HondaHRV extends Carro{
+
+    public HondaHRV(Motor motor) {
+        super(motor);
+        setModelo("HRV");
+        setCor(Color.BLACK);
+        setMontadora(Montadora.HONDA);
+    }
+
+}
